77663ab2b712c1b1f443947697429745a498c158
Quine Global <quine@fastmail.com>
2026-01-04 05:42:33 -0500

    Add fetch

--- src/frontend/src/components/SessionList.tsx
+++ src/frontend/src/components/SessionList.tsx
@@ -1,4 +1,5 @@
 import { createComponent } from 'react-fela'
+import { useQuery } from '@tanstack/react-query'
 
 const Container = createComponent(() => ({
 }))
@@ -9,8 +10,24 @@ type Session = {
 }
 
 export const SessionList = () => {
-  const sessions: Session[] = []
-  return <Container>{sessions.map((sess, i) => {
+  const { data, isPending, error } = useQuery<Session[]>({
+    queryKey: ['sessions'],
+    queryFn: async () => {
+      const r = await fetch('/api/sessions')
+      const j = await r.json()
+      return j as Session[]
+    },
+  })
+
+  if (isPending) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {`${error}`}</div>
+  }
+
+  return <Container>{(data ?? []).map((sess, i) => {
     return <div key={sess.id}>#{i} {sess.name ? `(${sess.name})` : ''}</div>
   })}</Container>
 }
