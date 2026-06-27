f1450263db378fe4bdc1b49c2ff1ed28fd09a2b2
Philip Peterson <1326208+philip-peterson@users.noreply.github.com>
2026-06-27 15:20:43 -0700

    Fix styles

--- src/frontend/src/components/Page.tsx
+++ src/frontend/src/components/Page.tsx
@@ -1,5 +1,5 @@
 export const Page = ({ children }: React.PropsWithChildren) => (
-  <div className="w-full min-h-screen overflow-auto" style={{ background: '#14002d' }}>
+  <div className="w-full h-screen overflow-auto" style={{ background: '#14002d' }}>
     {children}
   </div>
 )
