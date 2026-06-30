9416ff726e58d5ffb756d0b64c51afd15017bba0
Philip Peterson <1326208+philip-peterson@users.noreply.github.com>
2026-06-29 22:03:07 -0700

    Squint

--- fixtures/by-user/peterson@sent.com/~/Desktop/squint-editor.gapp/app.cljs
+++ fixtures/by-user/peterson@sent.com/~/Desktop/squint-editor.gapp/app.cljs
@@ -2,12 +2,18 @@
 
 (def SAVE_TIMEOUT_MS 15000)
 
-(def state (atom {:saving false :filename "Untitled.txt"}))
+(def state (atom {:saving false :opening false :filename "Untitled.txt"}))
 
 (def $editor (.getElementById js/document "editor"))
 (def $filename (.getElementById js/document "filename"))
 (def $status (.getElementById js/document "statusbar"))
 (def $save (.getElementById js/document "save-btn"))
+(def $open (.getElementById js/document "open-btn"))
+(def $picker (.getElementById js/document "file-picker"))
+(def $file-list (.getElementById js/document "file-list"))
+(def $picker-open (.getElementById js/document "picker-open-btn"))
+(def $picker-cancel (.getElementById js/document "picker-cancel-btn"))
+(def $picker-status (.getElementById js/document "picker-status"))
 
 (defn post! [msg]
   (.postMessage js/window.parent msg "*"))
@@ -24,7 +30,43 @@
 
 (defn render! []
   (set! (.-disabled $save) (:saving @state))
+  (set! (.-disabled $open) (:opening @state))
   (set! (.-textContent $filename) (:filename @state)))
+
+(defn show-picker! []
+  (set! (.-display (.-style $picker)) "flex")
+  (set! (.-textContent $picker-status) ""))
+
+(defn hide-picker! []
+  (set! (.-display (.-style $picker)) "none"))
+
+(defn open! []
+  (when-not (:opening @state)
+    (swap! state assoc :opening true)
+    (render!)
+    (set! (.-innerHTML $file-list) "")
+    (set! (.-textContent $picker-status) "Loading…")
+    (show-picker!)
+    (post! #js {:type "fs:browse"})))
+
+(defn populate-picker! [entries]
+  (set! (.-innerHTML $file-list) "")
+  (set! (.-textContent $picker-status) "")
+  (let [files (filter #(= "file" (.-type %)) entries)]
+    (if (empty? files)
+      (set! (.-textContent $picker-status) "No files on Desktop")
+      (doseq [f files]
+        (let [opt (.createElement js/document "option")]
+          (set! (.-value opt) (.-id f))
+          (set! (.-textContent opt) (.-name f))
+          (.appendChild $file-list opt))))))
+
+(defn picker-open! []
+  (let [val (.-value $file-list)]
+    (when (not= "" val)
+      (let [file-id (js/parseInt val 10)]
+        (set! (.-textContent $picker-status) "Opening…")
+        (post! #js {:type "fs:read" :fileId file-id})))))
 
 (defn load! [msg]
   (set-content! (.-content msg))
@@ -83,6 +125,20 @@
                    (swap! state assoc :saving false)
                    (render!)
                    (status! (.-message msg) true))
+    "fs:browse:complete" (do
+                           (populate-picker! (.-entries (.-result msg))))
+    "fs:browse:error" (do
+                        (swap! state assoc :opening false)
+                        (render!)
+                        (hide-picker!)
+                        (status! (or (.-message msg) "Browse failed") true))
+    "fs:read:complete" (let [r (.-result msg)]
+                         (swap! state assoc :opening false)
+                         (hide-picker!)
+                         (load! #js {:filename (.-name r) :content (.-content r)})
+                         (status! (str "Opened " (.-name r)) false))
+    "fs:read:error" (do
+                      (set! (.-textContent $picker-status) (or (.-message msg) "Could not open file")))
     nil))
 
 (defn on-kernel-message! [event]
@@ -91,6 +147,12 @@
       (handle-msg msg))))
 
 (.addEventListener $save "click" #(save!))
+(.addEventListener $open "click" #(open!))
+(.addEventListener $picker-open "click" #(picker-open!))
+(.addEventListener $picker-cancel "click" (fn []
+                                             (swap! state assoc :opening false)
+                                             (render!)
+                                             (hide-picker!)))
 (.addEventListener js/window "message" on-kernel-message!)
 (.addEventListener js/document "keydown"
   (fn [e]
